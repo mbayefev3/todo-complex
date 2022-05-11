bbd9cdcbc7a25a0c710b83a9f076eccd7ccfbe70
mbaye <mbaye@february3@yahoo.com>
2022-05-11 16:19:53 -0700

    first commit

---
+++ src/components/Title.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { Input } from 'semantic-ui-react'
+// import 'semantic-ui-css'
+
+const InputExampleInput = () => <Input placeholder='Search...' />
+
+export default InputExampleInput
